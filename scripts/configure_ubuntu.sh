67723a7a4bf59a5669b1706c5289ad957b43cc50
Novikov Nikolay <N.Novikov@transtelematica.ru>
2024-02-20 13:16:40 +0400

    Добавил скачивание стандартных пакетов в скрипт настройки Ubuntu.

--- scripts/configure_ubuntu.sh
+++ scripts/configure_ubuntu.sh
@@ -28,28 +28,51 @@ DEVIDER=": -------- : -------- : -------- : -------- : -------- :"
 
 #----------------------------------------------------------------------------------------------#
 #   Конфигурация. Только строка "enable" означает, что компонент будет выполняться.
+#   Закоммнтировать ненужное.
 #----------------------------------------------------------------------------------------------#
 
-# Обновление базовых пакетов системы
-FLAG_UPDATE_PACKAGES="disable"
-
-# Настройка окружения
-FLAG_SET_ENVIRONMENT="disable"
-
-# Настройка красивостей
-FLAG_MAKE_BEAUTY="disable"
-
+# FLAG_UPDATE_PACKAGES="enable"         # Обновление базовых пакетов системы
+FLAG_ADDITIONAL_PACKAGES="enable"     # Установка дополнительных пакетов
+# FLAG_SET_ENVIRONMENT="enable"         # Настройка окружения
+# FLAG_MAKE_BEAUTY="enable"             # Настройка красивостей
 
 #----------------------------------------------------------------------------------------------#
 #   Список пакетов для установки
 #----------------------------------------------------------------------------------------------#
 
-# apt
-# "git"
+# Пакеты для C/C++
+PACKAGES_C_CPP=(
+    gcc                     # Компилятор для языка C
+    g++                     # Компилятор для языка C++
+    make                    # Низкоуровневая система сборки
+    cmake                   # Высокоуровневая система сборки
+    mpich                   # Параллельные многоядерные вычисления
+    libgsl-dev              # Математические вычисления на C
+)
 
+# Пакеты для разработки
+PACKAGES_DEVELOPMENT=(
+    git                     # Система контроля версия
+    valgrind                # Утилита поиска утечек памяти
+    gdb                     # Отладчик
+    wireshark               # Сниффер трафика на сетевых интерфейсах
+    sqlitebrowser           # Посмотреть в sqlite БД
+    meld                    # Сравнивалка файлов
+)
 
+# Мультимедиа пакеты
+PACKAGES_MULTIMEDIA=(
+    vlc                     # Медиапроигрыватель
+)
+
+# Пакеты для общения
+PACKAGES_COMMUNICATION=(
+
+)
 
 # vscode
+# telegram
+# discord
 
 
 #----------------------------------------------------------------------------------------------#
@@ -93,6 +116,23 @@ function EXECUTE
     LOG "... \"$1\" success!"
 }
 
+# Установка пакета из стандартного репозитория через apt
+function INSTALL_PACKAGE
+{
+    LOG "Installing package \"$1\"..."
+
+        INSTALLED=$(apt list $1 2>>/dev/null | grep installed)
+
+        if [[ -z ${INSTALLED} ]]
+        then
+            echo -e "Required package \"$1\" is NOT INSTALLED"
+            echo -e "Installing...\n"
+            EXECUTE "sudo apt install -y $1"
+        else
+            LOG "Required package \"$1\" is INSTALLED"
+        fi
+}
+
 ################################################################################################
 #
 #   Конфигурация системы
@@ -104,7 +144,7 @@ function EXECUTE
 #   Обновление базовых пакетов системы.
 #----------------------------------------------------------------------------------------------#
 
-if [ "${FLAG_UPDATE_PACKAGES}" = "enable" ]
+if [ "${FLAG_UPDATE_PACKAGES}" ]
 then
 
     LOG_TITLE "Base system packages updating."
@@ -115,14 +155,49 @@ then
     # Обновление пакетов системы
     EXECUTE "sudo apt upgrade"
 
-fi # if [ "${FLAG_UPDATE_PACKAGES}" = "enable" ]
+fi # if [ "${FLAG_UPDATE_PACKAGES}" ]
 
+
+#----------------------------------------------------------------------------------------------#
+#   Установка дополнительных пакетов.
+#----------------------------------------------------------------------------------------------#
+
+if [ "${FLAG_ADDITIONAL_PACKAGES}" ]
+then
+
+    LOG_TITLE "Additional packages installing."
+
+    # Пакеты для C/C++
+    for var in ${PACKAGES_C_CPP[*]}
+    do
+        INSTALL_PACKAGE ${var}
+    done
+
+    # Пакеты для разработки
+    for var in ${PACKAGES_DEVELOPMENT[*]}
+    do
+        INSTALL_PACKAGE ${var}
+    done
+
+    # Мультимедиа пакеты
+    for var in ${PACKAGES_MULTIMEDIA[*]}
+    do
+        INSTALL_PACKAGE ${var}
+    done
+
+    # Пакеты для общения
+    for var in ${PACKAGES_COMMUNICATION[*]}
+    do
+        INSTALL_PACKAGE ${var}
+    done
+
+fi # if [ "${FLAG_ADDITIONAL_PACKAGES}" ]
 
 #----------------------------------------------------------------------------------------------#
 #   Настройка окружения
 #----------------------------------------------------------------------------------------------#
 
-if [ "${FLAG_SET_ENVIRONMENT}" = "enable" ]
+if [ "${FLAG_SET_ENVIRONMENT}" ]
 then
 
     LOG_TITLE "Environment settings."
@@ -168,14 +243,14 @@ set_prompt_line
     LOG "Changing console welcome string and add current git branch for user \"root\""
     sudo echo "${ADD_GIT_BRANCH}" >> /root/.bashrc
 
-fi # if [ "${FLAG_SET_ENVIRONMENT}" = "enable" ]
+fi # if [ "${FLAG_SET_ENVIRONMENT}" ]
 
 
 #----------------------------------------------------------------------------------------------#
 #   Настройка красивостей
 #----------------------------------------------------------------------------------------------#
 
-if [ "${FLAG_MAKE_BEAUTY}" = "enable" ]
+if [ "${FLAG_MAKE_BEAUTY}" ]
 then
 
     LOG_TITLE "Some beauty settings."
@@ -194,4 +269,4 @@ then
         EXECUTE "wget --no-check-certificate -O ${WP_DIR}/${WP_FILE} ${WP_URL}"
     fi
 
-fi # if [ "${FLAG_UPDATE_PACKAGES}" = "enable" ]
+fi # if [ "${FLAG_UPDATE_PACKAGES}" ]
